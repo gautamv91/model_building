2e52b052efbe141a4bedb58d3995bb85c033719c
gautamv91 <gautam.10791@gmail.com>
2026-04-02 07:53:56 +0530

    Updated the README file and the setup file to accommodate install through pip.

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
-with open("README.md", 'r') as f:
+with open("README.md", 'r', encoding='utf-8') as f:
     long_description = f.read()
 
 setup(
@@ -8,12 +8,17 @@ setup(
    version='1.0.0',
    description='The package contains useful APIs for performing exploratory data analysis, feature engineering & model building.',
    long_description=long_description,
+   long_description_content_type='text/markdown',
    author='Gautam Varadarajan',
    author_email='gautam.10791@gmail.com',
-   packages=['model_building'],  # same as name
-   install_requires=['pandas>=2.2.2',
-                     'numpy>=1.24.4',
-                     'matplotlib>=3.8.3',
-                     'seaborn>=0.13.2',
-                     'scikit-learn>=1.4.2']  # external packages as dependencies
+   url='https://github.com/gautamv91/model_building',
+   packages=find_packages(),
+   python_requires='>=3.8',
+   install_requires=[
+       'pandas>=2.2.2',
+       'numpy>=1.24.4',
+       'matplotlib>=3.8.3',
+       'seaborn>=0.13.2',
+       'scikit-learn>=1.4.2',
+   ]
 )
